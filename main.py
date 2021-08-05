37abbab8ce16f1d58bbe5b6f84cf5f09a996193d
Thai Le Quang <tleq@Thais-MacBook-Pro.local>
2021-08-05 12:47:57 +0700

    update

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from PIL import ImageGrab
 from time import sleep
 import pynput
 from copy import deepcopy
-
 import solver
 
-def get_img():
-    im2 = ImageGrab.grab(bbox=(335, 482, 1280, 1427))
+x1, y1 = 275, 482
+x2, y2 = 1220, 1427
+center_first_cell = 165, 268
+
+
+def get_img(x1, y1, x2, y2):
+    im2 = ImageGrab.grab(bbox=(x1, y1, x2, y2))
     im2.save("board.png")
+
 
 def img2array():
     board = [
@@ -32,46 +37,56 @@ def img2array():
     method = cv2.TM_CCOEFF_NORMED
     thresh_hold = .9
 
-    # cv2.imshow("original", img)
     for i in range(9):
         w, h = templates[i].shape[::-1]
         res = cv2.matchTemplate(img_gray, templates[i], method)
         loc = np.where(res >= thresh_hold)
         for pt in zip(*loc[::-1]):
-            # cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+            cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
             board[(pt[1] + h) // 105][(pt[0] + w) // 105] = i + 1
-    # cv2.imshow("after", img)
+    cv2.imwrite('after.png', img)
     return board
 
-def get_cells_pos():
+
+def get_cells_pos(center_first_cell, gap=52):
     cells_pos = []
     for col in range(9):
         cells_pos.append([])
         for row in range(9):
-            cells_pos[col].append([200 + row * 50, 280 + col * 50])
+            cells_pos[col].append([center_first_cell[0] + row * gap,
+                                   center_first_cell[1] + col * gap])
     return cells_pos
 
 
-if __name__ == '__main__':
-    get_img()
-    board = img2array()                         # -> chuyển hình sang mảng
-
+def main():
+    # Get and solve board
+    get_img(x1, y1, x2, y2)
+    board = img2array()
     result = deepcopy(board)
-    solver.solveSudoku(result)                  # -> giải
+    solver.solveSudoku(result)
 
+    # Initialize mouse and keyboard
     mouse = pynput.mouse.Controller()
     keyboard = pynput.keyboard.Controller()
 
-    cells_pos = get_cells_pos()                 #
-    mouse.position = cells_pos[0][0]            #
-    mouse.click(pynput.mouse.Button.left, 1)    # -> click qua tab game
-    sleep(0.2)                                  #
+    # Get all blank cell positions
+    cells_pos = get_cells_pos(center_first_cell)
+    mouse.position = cells_pos[0][0]
+    sleep(0.1)
+    mouse.click(pynput.mouse.Button.left, 1)
+    sleep(0.1)
 
+    # Automation
     for row in range(9):
         for col in range(9):
             if board[row][col] == 0:
                 mouse.position = cells_pos[row][col]
-                sleep(0.15)
+                sleep(0.27)
                 mouse.click(pynput.mouse.Button.left, 1)
                 num = result[row][col]
                 keyboard.press(str(num))
+                sleep(0.1)
+
+
+if __name__ == '__main__':
+    main()
